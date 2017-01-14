dd437f7d6b1c9f7d95dd482989a9e1dd87df6f64
itonis <madcrazy333@gmail.com>
2017-01-15 02:08:56 +0800

    Create BPEntry.h

---
+++ BPTree/BPEntry.h
@@ -0,0 +1,15 @@
+#include "IndexPageManager.h"
+
+#ifndef BP_ENTRY_H
+#define BP_ENTRY_H
+
+#pragma pack(push)
+#pragma pack(1)
+template<class Key>
+class BPEntry {
+    Key key;
+    ipg_pntr value;
+};
+#pragma pack(pop)
+
+#endif
