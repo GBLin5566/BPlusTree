59f4bfe5d0be0f18fd2e884d5f1a251b42c50979
GBLin <r04943092@ntu.edu.tw>
2017-01-18 01:40:09 +0800

    [DPM] Return record's info. to store.

--- Record.h
+++ Record.h
@@ -4,6 +4,11 @@
 
 template <class Key>
 class Record {
+    public:
+    int getSize(){return size;}
+    Key getKey(){return key;}
+    char* getRest(){return rest;}
+    private:
     int size;
     Key key;
     char *rest;
