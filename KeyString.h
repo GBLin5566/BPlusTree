fe2638210edc157e559c5888a321e46e3d376dc8
itonis <madcrazy333@gmail.com>
2017-01-19 17:21:33 +0800

    Add files via upload

--- KeyString.h
+++ KeyString.h
@@ -7,12 +7,17 @@
 class KeyString {
     public:
     unsigned char str[KEY_STRING_LEN];
+    KeyString();
     KeyString(std::string s);
+    KeyString operator=(const KeyString rhs);
     bool operator<(const KeyString right);
     bool operator>=(const KeyString right);
     bool operator==(const KeyString right);
     friend std::ostream& operator<<(std::ostream&, const KeyString&);
 };
+KeyString::KeyString() {
+    memset(str, 0, KEY_STRING_LEN);
+}
 KeyString::KeyString(std::string s) {
     int len = KEY_STRING_LEN;
     if (s.length() < len) {
@@ -24,6 +29,10 @@ KeyString::KeyString(std::string s) {
     for (int i=len; i<KEY_STRING_LEN; i++) {
         str[i]=0;
     }
+}
+inline KeyString KeyString::operator=(const KeyString rhs) {
+    memcpy( str, rhs.str, KEY_STRING_LEN);
+    return *this;
 }
 inline bool KeyString::operator>=(const KeyString right) {
     for(int i=0; i<KEY_STRING_LEN; i++) {
